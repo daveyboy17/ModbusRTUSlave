70c564baef8732e4c118d8387a1f888e0c881547
daveyboy17 <daveyboy17nz@gmail.com>
2026-08-06 22:27:20 +1200

    Cleanup of commented code.

--- app/protocol/test_crc16.py
+++ app/protocol/test_crc16.py
@@ -1,5 +1,4 @@
 import unittest
-# from .crc16 import calculate
 from .crc16 import calculate
 
 
--- app/protocol/test_decoder.py
+++ app/protocol/test_decoder.py
@@ -1,5 +1,4 @@
 import unittest
-# from .decoder import describe
 from .decoder import describe
 
 
