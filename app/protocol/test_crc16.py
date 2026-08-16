d8ea4e47f623dca25d572443acf03c5ace6b816e
daveyboy17 <daveyboy17nz@gmail.com>
2026-08-16 21:34:48 +1200

    Added tests to test_crc16.py

--- app/protocol/test_crc16.py
+++ app/protocol/test_crc16.py
@@ -2,8 +2,20 @@ import unittest
 from .crc16 import calculate
 
 
+# request 01 03 00 00 00 02 C4 0B
+# response 01 03 04 00 64 00 C8 BA 7A
+
+
 class TestCrc16(unittest.TestCase):
     def test_ValidFrames(self):
         frame = bytes([1, 3, 0, 1, 12])
         response = calculate(frame)
         self.assertEqual(response, 4376)
+        
+        frame = bytes([1, 3, 0, 0, 0, 2])
+        response = calculate(frame)
+        self.assertEqual(response, 50187)
+        
+        frame = bytes([1, 3, 4, 0, 100, 0, 200])
+        response = calculate(frame)
+        self.assertEqual(response, 47738)
